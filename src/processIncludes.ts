2c902d01ebe561fca6ec5268f99f36581bee9d2b
Callum Loh <callumloh@gmail.com>
2021-12-30 04:21:18 +0000

    Inject recusion and ESI headers into includes. Add blacklist thirdparty includes support

--- src/processIncludes.ts
+++ src/processIncludes.ts
@@ -1,12 +1,11 @@
-import { tagParser } from "./tagParser";
 import { replace_vars } from './processESIVars'
-import esi from ".";
+import esi, { ESIConfig } from ".";
 import { ESIEventData } from ".";
 
-let esi_include_pattern = /<esi:include\s*src="[^"]+"\s*\/>/
-let esi_src_pattern = /src="([^"]+)"/
+const esi_include_pattern = /<esi:include\s*src="[^"]+"\s*\/>/
+const esi_src_pattern = /src="([^"]+)"/
 
-export async function process(eventData: ESIEventData, chunk: string, evalVars: boolean) {
+export async function process(eventData: ESIEventData, chunk: string, evalVars: boolean): Promise<string> {
 
 	if (chunk.indexOf("<esi:include") == -1) {
 		if (evalVars) {
@@ -16,21 +15,21 @@ export async function process(eventData: ESIEventData, chunk: string, evalVars: 
 		}
 	}
 
-	let res = []
+	const res = []
 	let retFrom = 0
 
 	do {
 
 		chunk = chunk.substring(retFrom)
-		let includeMatch = chunk.match(esi_include_pattern)
+		const includeMatch = chunk.match(esi_include_pattern)
 
 		if (includeMatch && includeMatch.index !== undefined) {
-			let before = chunk.substring(retFrom, includeMatch.index)
+			const before = chunk.substring(retFrom, includeMatch.index)
 
 			retFrom = includeMatch.index + includeMatch[0].length;
 			res.push(evalVars ? replace_vars(eventData, before) : before)
 
-			let include = await fetchInclude(
+			const include = await fetchInclude(
 				eventData,
 				includeMatch[0]
 			)
@@ -43,6 +42,7 @@ export async function process(eventData: ESIEventData, chunk: string, evalVars: 
 			break;
 		}
 
+		// eslint-disable-next-line no-constant-condition
 	} while (true)
 
 	return res.join("")
@@ -50,7 +50,7 @@ export async function process(eventData: ESIEventData, chunk: string, evalVars: 
 
 async function fetchInclude(eventData: ESIEventData, include: string) {
 
-	let src = parseSrcAttribute(eventData, include)
+	const src = parseSrcAttribute(eventData, include)
 
 	if (!src) {
 		// No src. Cant do anything
@@ -58,28 +58,67 @@ async function fetchInclude(eventData: ESIEventData, include: string) {
 	}
 
 	// TY JS for handling relatives etc for us
-	let req = new Request(new URL(src, eventData.url.toString()).toString());
+	const srcUrl = new URL(src, eventData.url.toString())
+	const req = new Request(srcUrl.toString());
+	const sameDomain = isIncludeOnSameDomain(eventData.url, srcUrl)
 
-	// create a new parser
-	let parser = new esi();
-	let includeRes = await parser.parse(req, eventData.recursion + 1);
+	// Not the same domain are we even allowed to include this
+	if (!sameDomain && !thirdPartyWhitelisted(eventData.config, srcUrl.host)) {
+		return ""
+	}
+
+	// Add authorization and cookie data if on the same domain
+	if (sameDomain) {
+		if (eventData.request.headers.get('Authorization')) {
+			req.headers.set('Authorization', eventData.request.headers.get('Authorization') as string)
+		}
+		if (eventData.request.headers.get('Cookie')) {
+			req.headers.set('Cookie', eventData.request.headers.get('Cookie') as string)
+		}
+	}
+
+	// Add our recursion level and parent uri
+	req.headers.set('X-ESI-Recursion-Level', (eventData.recursion + 1).toString())
+	req.headers.set('X-ESI-Parent-URI', eventData.request.url)
+	if (eventData.request.headers.get('Cache-Control')) {
+		req.headers.set('Cache-Control', eventData.request.headers.get('Cache-Control') as string)
+	}
+
+	// create a new parser with current config
+	const parser = new esi(eventData.config);
+	const includeRes = await parser.parse(req, eventData.recursion + 1);
 
 	if (!includeRes.body) {
 		// Nothing for us to do here
 		return ""
 	}
 
-	let resTxt = await includeRes.text()
+	const resTxt = await includeRes.text()
 
 	return resTxt
 }
 
 function parseSrcAttribute(eventData: ESIEventData, include: string) {
-	let src = include.match(esi_src_pattern)
+	const src = include.match(esi_src_pattern)
 
 	if (!src) {
 		return null
 	}
 
 	return replace_vars(eventData, src[1])
+}
+
+function isIncludeOnSameDomain(requestURL: URL, srcURL: URL): boolean {
+	return srcURL.hostname == requestURL.hostname
+}
+
+function thirdPartyWhitelisted(config: ESIConfig, host: string): boolean {
+
+	if (config.disableThirdPartyIncludes) {
+		if (!config.thirdPatyIncludesDomainWhitelist) { return false }
+
+		return config.thirdPatyIncludesDomainWhitelist.includes(host)
+	}
+
+	return true
 }
